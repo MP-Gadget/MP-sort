968d259abca4788e54b8daf0dd0d167c59338863
Yu Feng <rainwoodman@gmail.com>
2016-08-07 10:01:35 -0700

    update check tag.

--- check_tag.sh
+++ check_tag.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+grep __version__ "$1"
+
 if [[ -n "$TRAVIS_TAG" ]]; then
-    if ! grep $TRAVIS_TAG setup.py; then
+    if ! grep $TRAVIS_TAG "$1"; then
         echo Tag $TRAVIS_TAG does not match setup.py version. Bail.
         exit 1
     fi
